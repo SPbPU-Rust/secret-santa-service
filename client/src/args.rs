681ab497e8d793eca28ce9c3d79ef57ebf69089f
Fascinat0r <fascinator2904@gmail.com>
2023-01-08 01:08:28 +0300

    Commit 1: Add client part except delete func

--- client/src/args.rs
+++ client/src/args.rs
@@ -13,12 +13,39 @@ pub struct SantaArgs {
 #[derive(Debug, Subcommand)]
 pub enum EntityType {
     Hi(Hello),
+    Create(CreateRoom),
+    Get(GetRoom),
+    Start(StartGame),
+    End(EndGame),
     Duser(DeleteUser),
     Droom(DeleteRoom)
 }
 
 #[derive(Debug, Args)]
 pub struct Hello {
+}
+
+#[derive(Debug, Args)]
+pub struct CreateRoom {
+    pub admin_token: String
+}
+
+#[derive(Debug, Args)]
+pub struct GetRoom {
+    pub room_id: String,
+    pub name: String
+}
+
+#[derive(Debug, Args)]
+pub struct StartGame {
+    pub room_id: String,
+    pub admin_token: String
+}
+
+#[derive(Debug, Args)]
+pub struct EndGame {
+    pub room_id: String,
+    pub admin_token: String
 }
 
 #[derive(Debug, Args)]
