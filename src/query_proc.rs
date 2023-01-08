074b17e09b4cda85d68912446b1c31eec31c85df
StarkAlive <st4rkmp@st4rkhplt>
2023-01-08 03:37:15 +0300

    night 20230108 draft refactoring and fixes (http responses in query_proc are not done yet)

--- src/query_proc.rs
+++ src/query_proc.rs
@@ -1,13 +1,17 @@
 use std::string::String;
 use serde_json::{Map, Value};
 
-use crate::auth;
 use crate::defs;
 use crate::state::*;
+use crate::auth;
+use crate::proc_for_users as gr_usr;
+use crate::proc_for_admin as gr_adm;
 
 pub(crate) fn process_req(req: Map<String, Value>, data_state: &mut DataState) -> (String, String) {
     let LACKING_FIELDS: (String, String) = (defs::HTTP_STATUS_400.to_string(), "{\"error\": \"Не все обязательные поля заполнены значениями требуемых форматов\"}".to_string());
     let UNSUPPORTED_OP: (String, String) = (defs::HTTP_STATUS_400.to_string(), "{\"error\":\"Неподдерживаемая, невыполнимая или неверно сформированная операция\"}".to_string());
+    let INTERNAL_ERROR: (String, String) = (defs::HTTP_STATUS_500.to_string(), "{\"error\":\"Не удалось выполнить операцию из-за неполадок на сервере\"}".to_string());
+    let NOT_FOUND: (String, String) = (defs::HTTP_STATUS_404.to_string(), "Не удалось найти информацию о запрашиваемом объекте".to_string());
     // Перед считыванием значения по ключу обязательно проверять, что пара с желаемым ключом есть в структуре, чтобы не было вылета
     /* // шаблонные примеры - как считывать значения пар "ключ:значение"
     if req.contains_key("sat") {
@@ -29,15 +33,25 @@ pub(crate) fn process_req(req: Map<String, Value>, data_state: &mut DataState) -
                     (s, c) = UNAUTHORIZED_OP;
                 } else {
                     (s, c) = match req["action"].as_str() {
-                        Some("new_group") => { // TODO://FIXME: заглушка
-                            (defs::HTTP_STATUS_403.to_string(), "{\"error\":\"Запрещено\"}".to_string())
+                        Some("new_group") => {
+                            match req.contains_key("name") && req["name"].is_string() {
+                                true => {
+                                    let gid = gr_usr::make_group(uid, req["name"].as_str().unwrap().to_string(), data_state);
+                                    match gid {
+                                        0 => INTERNAL_ERROR,
+                                        _ => (defs::HTTP_STATUS_200.to_string(), "{\"id\":\"".to_string() + &gid.to_string() + "\"}")
+                                    }
+                                },
+                                false => LACKING_FIELDS
+                            }
                         },
                         Some("logout") => {
                             match auth::logout(req["sat"].as_str().unwrap().to_string(), data_state) {
                                 true => (defs::HTTP_STATUS_200.to_string(), "{\"msg\":\"Сеанс завершен\"}".to_string()),
                                 false => (defs::HTTP_STATUS_500.to_string(), "{\"error\": \"Не найден сеанс для завершения (ошибка сервера)\"}".to_string())
                             }
-                        }
+                        },
+                        //Some("")
                         _ => UNSUPPORTED_OP
                     };
                 }
@@ -93,7 +107,35 @@ pub(crate) fn process_req(req: Map<String, Value>, data_state: &mut DataState) -
                 Some("group_stat") => {
                     match req.contains_key("gid") && req["gid"].is_u64() && req["gid"].as_u64().unwrap() > 0 {
                         true => {
-                            ("".to_string(), "".to_string()) // FIXME: заглушка
+                            let mut exists: bool = false;
+                            let gid = req["gid"].as_u64().unwrap();
+                            let mut group_name: String = "".to_string();
+                            let mut group_is_closed: bool = false;
+                            {
+                                let data_state_group_ref = &data_state.group;
+                                for rec in data_state_group_ref {
+                                    if rec.id == gid {
+                                        exists = true;
+                                        group_name = rec.name.clone();
+                                        group_is_closed = rec.is_closed;
+                                        break;
+                                    }
+                                }
+                            }
+                            match exists {
+                                true => {
+                                    let urecs = gr_usr::list_users_in_group_recs(gid, data_state);
+                                    let mut resp_arr: String = "{\"name\":\"".to_string() + &group_name +
+                                        "\", \"is_closed\": " + &group_is_closed.to_string() + ", \"members\":[";
+                                    for urec in urecs {
+                                        resp_arr += &("{\"uid\":".to_string() + &urec.uid.to_string() +
+                                            ", \"is_admin\": " + &urec.is_admin.to_string() + "}");
+                                    }
+                                    resp_arr += "]}";
+                                    (defs::HTTP_STATUS_200.to_string(), "".to_string())
+                                },
+                                false => NOT_FOUND
+                            }
                         },
                         false => LACKING_FIELDS
                     }
